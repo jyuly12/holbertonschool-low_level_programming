fefef1337a2c64bba80e19f49e800fed08a576e2
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-02-04 22:18:30 +0000

    print alphabet less e,q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char t;
+for (t = 'a'; t <= 'z'; t++)
+if (t != 'e' && t != 'q')
+putchar(t);
+putchar('\n');
+return (0);
+}
