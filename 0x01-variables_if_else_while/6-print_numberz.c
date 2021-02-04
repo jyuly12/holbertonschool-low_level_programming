3651bbbd2791e54796acd6a17ae88fd0d331a198
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-02-04 22:23:54 +0000

    print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int t;
+for (t = '0'; t <= '9'; t++)
+putchar(t);
+putchar('\n');
+return (0);
+}
