a535dd7c3d5a51e58c12b60147bda1fd82945e24
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-02-04 03:30:04 +0000

    low level programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
-#include<unistd.h>
+#include <stdio.h>
+#include <unistd.h>
 
 
  /**
@@ -8,6 +9,6 @@
   */
 int main(void)
 {
-write(2, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", );
+write(2, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
