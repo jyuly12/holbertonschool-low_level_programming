560c2abe96ce51d8b7e57ecb2ff2c3414ff6bcc1
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-02-04 20:33:34 +0000

    print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,12 +5,14 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 char t;
-
-for(t = 'a'; t <= 'z'; t++);
+for (t = 'a'; t <= 'z'; t++)
+{
 putchar(t);
 putchar('\n');
+}
 return (0);
 }
