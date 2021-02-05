a7ff23b2239f13661d18752bb243b57aa520f4d9
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-02-05 00:53:16 +0000

    print all possible combinations of single-digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,17 +1,17 @@
+#include <stdio.h>
+
 /**
  * main - Entry point
  *
- * Retur'n: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
-#include <stdio.h>
-
 int main(void)
 {
 int c;
-for (c = 48; c <= 57; c++)
+for (c = '0'; c <= '9'; c++)
 {
 putchar(c);  
-if (c < 57)
+if (c < '9')
 {
 putchar(',');
 putchar(' ');
