9f1bb794e95a5605e1f7a4a27df6d51bd78ce0d0
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-02-05 00:35:30 +0000

    print all possible combinations of single-digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+/**
+ * main - Entry point
+ *
+ * Retur'n: Always 0 (Success)
+ */
+#include <stdio.h>
+
+int main(void)
+{
+int c;
+for (c = 48; c <= 57; c++)
+{
+putchar(c);  
+if (c < 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
