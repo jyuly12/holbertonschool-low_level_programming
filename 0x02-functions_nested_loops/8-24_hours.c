e756393f8a8df9c6ee07d3c491260585b7956441
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-02-09 17:09:38 +0000

    proyect completed

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,11 +7,13 @@ void jack_bauer(void)
 int m, h, t, s;
 for (h = 48; h <= 50; h++)
 {
-for (t = 48; t <= 51; t++)
+for (t = 48; t <= 57; t++)
 {
 for (m = 48; m <= 53; m++)
 {
 for (s = 48; s <= 57; s++)
+{
+if (h < 50 || t <= 51)
 {
 _putchar(h);
 _putchar(t);
@@ -19,6 +21,7 @@ _putchar(':');
 _putchar(m);
 _putchar(s);
 _putchar('\n');
+}
 }
 }
 }
