125584ce56a800dc20c8b5f6bf119740750c3b7b
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-02-09 14:48:51 +0000

    prints Holberton

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,9 @@
+#include "holberton.h"
+
+void print_alphabet(void)
+{
+char j;
+for(j = 'a'; j <= 'z'; j++)
+_putchar(j);
+_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+int _abs(int c)
+{
+if (c < 0)
+{
+c = c * -1;
+return (c);
+}
+else
+{
+return (c);
+}
+}
