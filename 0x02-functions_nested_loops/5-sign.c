c31ff325b1d29c44a1daf63a222b2c0cb13950aa
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-02-09 16:12:06 +0000

    Sign complete

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,5 +1,11 @@
 #include "holberton.h"
-
+/**
+ * print_sign - prints the sign of a number.
+ *
+ * @n: The character to be checked.
+ *
+ * Return: Always 0.
+ */
 int print_sign(int n)
 {
 if (n > 0)
