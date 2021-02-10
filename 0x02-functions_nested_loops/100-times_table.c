26d356163972217b86780fb6bdbb485f29a4c40c
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-02-10 01:03:39 +0000

    avanced task

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,7 +8,9 @@
  */
 void print_times_table(int n)
 {
-  int i, j, k, c, s;
+  int i, j, k, c, s, l;
+  if (n <= 15 && n >= 0)
+    {
   for (i = 0; i <= n; i++)
     {
       for (j = 0; j <= n; j++)
@@ -16,6 +18,7 @@ void print_times_table(int n)
 	  k = i * j;
 	  c = k / 10;
 	  s = k % 10;
+	  l = (k /100) % 10;
 	  if (k < 10)
 	    {
 	      if (j != 0)
@@ -33,7 +36,15 @@ void print_times_table(int n)
 	      _putchar(c + '0');
 	      _putchar(s + '0');
 	    }
+	  else {
+	    _putchar(',');
+	    _putchar(' ');
+	    _putchar(l + '0'); 
+	    _putchar(c + '0');
+	    _putchar(s + '0');
+	  }
 	}
       _putchar('\n');
     }
+}
 }
