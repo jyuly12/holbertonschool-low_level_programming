a7ff2d16d55efb29986d2c67ae89ca39a1fe632c
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-02-10 00:53:43 +0000

    avanced task

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,42 +6,34 @@
  *
  * Return: Void.
  */
-int i, j, k, c, s, m;
 void print_times_table(int n)
 {
-if (n >= 0 && n <= 15)
-{
-for (i = 0; i <= n; i++)
-{
-for (j = 0; j <= n; j++)
-{
-k = i * j;
-c = k / 10;
-s = k % 10;
-m = k / 100;	      
-if (k < 10 && j != 0)
-{
-_putchar(',');
-_putchar(' ');
-_putchar(s + '0');
-}
-else if (k >= 10 && k < 100) 
-{
-_putchar(',');
-_putchar(' ');
-_putchar(c + '0');
-_putchar(s + '0');
-}
- else   
-{
-_putchar(',');
-_putchar(' ');
-_putchar(m + '0');
-_putchar(c % 10 + '0');
-_putchar(s + '0');
-}
-_putchar('\n');
-}
-}
-}
+  int i, j, k, c, s;
+  for (i = 0; i <= n; i++)
+    {
+      for (j = 0; j <= n; j++)
+	{
+	  k = i * j;
+	  c = k / 10;
+	  s = k % 10;
+	  if (k < 10)
+	    {
+	      if (j != 0)
+		{
+		  _putchar(',');
+		  _putchar(' ');
+		  _putchar(' ');
+		}
+	      _putchar(s + '0');
+	    }
+	  else if (k >= 10 && k < 100)
+	    {
+	      _putchar(',');
+	      _putchar(' ');
+	      _putchar(c + '0');
+	      _putchar(s + '0');
+	    }
+	}
+      _putchar('\n');
+    }
 }
