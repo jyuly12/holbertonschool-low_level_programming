f6408bf98dba27c7c2f1406e7d39b0681a18fd6c
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-02-10 01:52:42 +0000

    avanced task

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,7 +2,7 @@
 /**
  * print_times_table - prints the n times table, starting with 0.
  *
- *
+ * @n: The character to be checked.
  *
  * Return: Void.
  */
@@ -10,7 +10,6 @@ void print_times_table(int n)
 {
 int i, j, k, c, s, l;
 if (n <= 15 && n >= 0)
-{
 for (i = 0; i <= n; i++)
 {
 for (j = 0; j <= n; j++)
@@ -18,7 +17,7 @@ for (j = 0; j <= n; j++)
 k = i * j;
 c = k / 10;
 s = k % 10;
-l = (k /100) % 10;
+l = (k / 100) % 10;
 if (k < 10)
 {
 if (j != 0)
@@ -38,15 +37,14 @@ _putchar(' ');
 _putchar(c + '0');
 _putchar(s + '0');
 }
-else {
+else
+{
 _putchar(',');
 _putchar(' ');
-_putchar(l + '0'); 
+_putchar(l + '0');
 _putchar(c % 10 + '0');
 _putchar(s + '0');
 }
 }
 _putchar('\n');
-}
-}
-}
+}}
