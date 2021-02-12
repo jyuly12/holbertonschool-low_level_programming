5a81332c1ebb5d0fb8a20c80145743fa6cf6adfb
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-02-12 23:19:07 +0000

    task complete

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - Entry point.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int n;
+long num = 612852475143;
+for (n = (int) sqrt(num); n > 2; c++)
+{
+if (num % n == 0)
+{
+printf("%d\n", n);
+break;
+}
+}
+return (0);
+}
