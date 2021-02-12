b9d40eb075c24bb291f4fe3f2b123704c616ad68
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-02-12 23:55:37 +0000

    task complete

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_number - Prints an integer.
+ * @n: The integer.
+ *
+ */
+void print_number(int n)
+{
+unsigned int numb = n;
+if (n < 0)
+{
+_putchar('-');
+numb = -numb;
+}
+if ((numb / 10) > 0)
+print_number(numb / 10);
+_putchar((numb % 10) + '0');
+}
