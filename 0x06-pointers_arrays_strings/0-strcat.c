cffec282d243c4599d0b1dd243d7c5c623ff8e76
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-02-18 17:26:09 +0000

    task complete

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,14 +1,14 @@
 #include "holberton.h"
-
 /**
- * _strcat - Concatenates two strings. 
+ * _strcat - Concatenates two strings.
  * @dest: string value.
  * @src: string value.
  *
+ * Return: Always dest string.
  */
 char *_strcat(char *dest, char *src)
 {
-int i = 0, j = 0; 
+int i = 0, j = 0;
 while (dest[i] != '\0')
 {
 i++;
@@ -19,7 +19,6 @@ dest[i] = src[j];
 i++;
 j++;
 }
-dest[i] = '\0'; 
-
-return (0);  
+dest[i] = '\0';
+return (dest);
 }
