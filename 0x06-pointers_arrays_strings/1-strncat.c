6983ac25e0cdcb325d86023ce4acae9d9014ff4f
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-02-18 19:54:44 +0000

    task complete

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * _strncat - Concatenates two strings.
+ * @dest: string value.
+ * @src: string value.
+ * @n: parameter value.
+ *
+ * Return: Always dest string.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int i = 0, j = 0;
+while (dest[i] != '\0')
+{
+i++;
+}
+while (src[j] != '\0' && j < n)
+{
+dest[i] = src[j];
+j++;
+i++;
+}
+dest[i] = '\0';
+return (dest);
+}
