1ad3003c8da30832119f159e55adecf0161c8dc1
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-02-18 21:19:00 +0000

    task complete

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,11 +1,15 @@
 #include "holberton.h"
+
 /**
+ * _strncpy - Function that copies a string.
  *
+ * @dest: string value.
+ * @src: string value.
+ * @n: parameter value.
  *
- *
- *
- *
+ * Return: Always dest string.
  */
+
 char *_strncpy(char *dest, char *src, int n)
 {
 int i = 0, j = 0;
@@ -16,12 +20,11 @@ dest[i] = src[j];
 j++;
 i++;
 }
-  
+
 while (i < n)
-{ 
+{
 dest[i] = '\0';
 i++;
 }
 return (dest);
 }
-
