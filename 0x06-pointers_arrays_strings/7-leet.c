f1d849c58d68c39d5c7273a20d30b6f07a0d8d77
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-02-19 02:44:40 +0000

    task complete

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,32 +10,21 @@
 
 char *leet(char *str)
 {
-int i = 0;
+int i = 0, j = 0;
+char letter[] = {AaEeOoTtLl}
+char numbers[] = {4433007711}
 
 while (str[i] != '\0')
 {
-if (str[i] == 65 || str[i] == 97)
+while (j <= 4)
 {
-str[i] = 52;
+if (str[i] == letter[j])
+{
+str[i] = numbers[j];
 }
-if (str[i] == 69 || str[i] == 101)
-{
-str[i] = 51;
-}
-if (str[i] == 71 || str[i] == 111)
-{
-str[i] = 48;
-}
-if (str[i] == 84 || str[i] == 116)
-{
-str[i] = 55;
-}
-if (str[i] == 76 || str[i] == 108)
-{
-str[i] = 49;
+j++;
 }
 i++;
 }
-str[i] = '\0';
 return (str);
 }
