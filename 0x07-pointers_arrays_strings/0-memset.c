e16db67468c06b818d45a0bc41bd9c0e4c55a908
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-02-22 17:18:10 +0000

    task complete

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,10 +1,11 @@
 #include "holberton.h"
 /**
+ * _memset - Fills memory with a constant byte.
+ * @s: the address of memory to print.
+ * @b: constant byte.
+ * @n: bytes of the memory area pointed.
  *
- *
- *
- *
- *
+ * Return: Always @s.
  */
 char *_memset(char *s, char b, unsigned int n)
 {
