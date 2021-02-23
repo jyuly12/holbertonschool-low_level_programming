08efecd57689767784d5e9c9a8a75e35f64007ba
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-02-23 22:04:03 +0000

    task complete

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+
+void print_diagsums(int *a, int size)
+{
+int i, j, count, first, second;
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+if(i == j)
+{
+first += a[count];
+}
+if((i + j) == (size - 1))
+{
+second += a[count];
+}
+count++;
+}
+}
+printf("%d, %d\n", first, second);
+}
