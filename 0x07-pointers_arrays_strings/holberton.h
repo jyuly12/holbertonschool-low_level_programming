f66b5a1a0a82d5dc235223898a30969f647770c4
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-02-23 04:13:41 +0000

    header file

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -6,7 +6,9 @@
  * Auth: Julieth Gonzalez.
  * Desc: Header file containing declarations for all functions
  *       used in the 0x07-pointers_arrays_strings
- */ 
+ */
+
+int _putchar(char c); 
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
