882a945c2c7032103ace8a48a51a84d4896c088e
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-02-23 02:36:51 +0000

    task complete

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * _strstr - locates a substring.
+ * @haystack: string to check.
+ * @needle: substring to locate.
+ *
+ * Return: Always needle value into haystack.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+int i = 0, j;
+
+while (haystack[i] != '\0')
+{
+for (j = 0; needle[j] != '\0'; j++)
+{
+if (needle[j] != haystack[i])
+{
+break;
+}
+else if (needle[j] == haystack[i])
+{
+return (haystack + i);
+}
+}
+i++;
+}
+return ('\0');
+}
