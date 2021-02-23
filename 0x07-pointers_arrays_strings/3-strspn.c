abc8e7d41840f50ad2f6f42b40ea383f0eea8da8
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-02-23 01:17:16 +0000

    task complete

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,14 +10,19 @@ unsigned int _strspn(char *s, char *accept)
 {
 int i, j;
 unsigned int k;
-for (i = 0; s[i] != ' '; i++)
+
+for (i = 0; accept[i] != '\0'; i++)
 {
-for (j = 0; accept[j] != '\0'; j++)
+for (j = 0; s[j] != '\0'; j++)
 {
-if (s[i] == accept[j])
+if (s[j] == accept[i])
 {
 k++;
 break;
+}
+if (accept[i + i] == '\0')
+{
+k++;
 }
 }
 }
