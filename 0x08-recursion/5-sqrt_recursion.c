57b43af552478fd851cfea60d712c856b89bfe13
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-02-24 16:46:23 +0000

    task complete

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,22 +1,12 @@
 #include "holberton.h"
 /**
- * _sqrt_recursion - Natural square root of a number.
- * @n: number to calculate.
- *
- * Return: square root.
- */
-int _sqrt_recursion(int n)
-{
-return _sqrt_value (n, 1);
-}
-/**
- * _sqrt_value - Natural square root of a number.
+ * sqrt_value - Natural square root of a number.
  * @i: number to calculate.
  * @j: parameter.
  *
  * Return: square root.
  */
-int _sqrt_value(int i, int j)
+int sqrt_value(int i, int j)
 {
 if (j * j == i)
 {
@@ -26,5 +16,17 @@ if (j * j > i)
 {
 return (-1);
 }
-return (_sqrt_value(i, j + 1));
+return (sqrt_value(i, j + 1));
 }
+
+/**
+ * _sqrt_recursion - Natural square root of a number.
+ * @n: number to calculate.
+ *
+ * Return: square root.
+ */
+int _sqrt_recursion(int n)
+{
+return (sqrt_value(n, 1));
+}
+
