983573025a40614d1472c4dd5e776b97705150f9
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-03-08 20:47:09 +0000

    header file

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,7 +1,7 @@
 #ifndef ABS_H
 #define ABS_H
 /*
- * computes the absolute value of a number x
+ * Computes the absolute value of a number x
  */
-#define ABS(x) (((x) < 0) ? (-(x) : (x))
+#define ABS(x) (((x) < 0) ? -(x) : (x))
 #endif
