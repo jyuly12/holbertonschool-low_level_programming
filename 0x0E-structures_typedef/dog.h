9a17223bdfccaa106e6a1b5343889ede9a013e1f
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-03-08 22:33:29 +0000

    correct dog.h header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,7 +2,6 @@
 #define DOG_H
 /**
  * struct dog - dog parameters
- *
  * @name: dog name.
  * @age: dog age.
  * @owner: dog owner.
@@ -12,10 +11,10 @@ struct dog
 char *name;
 float age;
 char *owner;
-}
+};
 
 typedef struct dog dog_t;
-int _putchar(char c);
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
