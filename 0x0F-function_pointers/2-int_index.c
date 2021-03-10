86bba6752fc9469ca791919c82c1b0fce83f0ea9
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-03-10 21:57:40 +0000

    task 2 complete

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,8 +10,10 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 int i;
-if (array != NULL || cmp != NULL)
+if (array == NULL || cmp == NULL)
 {
+return (-1);
+}
 for (i = 0; i < size; i++)
 {
 if (cmp(array[i]) != 0)
