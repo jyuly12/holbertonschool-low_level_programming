fed630a4decdcbe1faf0163ee1e5e06657d00653
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-03-11 03:59:03 +0000

    first upload

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,27 @@
+#include "3-calc.h"
+/**
+ * get_op_func - select the correct function.
+ * @s: input.
+ *
+ * Return: value.
+ */
+int (*get_op_func(char *s))(int, int)
+{
+op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
+int i = 0;
+while (ops[i] != NULL)
+{
+if (*s == *(ops[i].op) && *s < '\0')
+{
+return (ops[i].f);
+}
+i++;
+}
+}
