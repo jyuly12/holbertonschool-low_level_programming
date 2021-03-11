15387118838a2e5a132e41f4c60b6ac8fa2b1025
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-03-11 04:12:23 +0000

    incomplete

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,26 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - main function.
+ * @argc: size to function.
+ * @argv: string.
+ *
+ *
+ * Return: always 0.
+ */
+int main(int argc __attribute__((unused)), char argv[])
+{
+int i;
+if (argc != 4)
+{
+printf("Error\n");
+}
+else 
+atoi(argv[2]);
+get_op_func(argv[3]);
+atoi(argv[4]);
+
+printf()
+}
