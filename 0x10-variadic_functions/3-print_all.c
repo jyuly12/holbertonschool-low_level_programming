c6827f851e75ef76e4ba1ca59b790d9c5018d956
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-03-12 02:20:37 +0000

    task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,49 +3,47 @@
 /**
  * print_all - prints anything.
  * @format: list of types of arguments.
- *
- * Return: Nothing.
  */
 void print_all(const char *const format, ...)
 {
-unsigned int k;
+unsigned int k = 0, j = 0;
 char *ch;
 va_list ap;
-
 va_start(ap, format);
-k = 0;
-while (format[k] && format != NULL)
+while (format[k] != '\0' && format != NULL)
 {
 switch (format[k])
 {
 case 'i':
 printf("%d", va_arg(ap, int));
+j = 1;
 break;
 case 'f':
 printf("%f", va_arg(ap, double));
+j = 1;
 break;
 case 'c':
 printf("%c", va_arg(ap, int));
+j = 1;
 break;
 case 's':
+j = 1;
 ch = va_arg(ap, char *);
 if (ch == NULL)
 {
-printf("(nil)");
-break;
+ch = "(nil)";
 }
 printf("%s", ch);
 break;
 default:
-k++;
-continue;
+j = 0;
+break;
 }
-if (format[k + 1] != '\0')
-{
+if (format[k + 1] != '\0' && j)
 printf(", ");
-}
+
 k++;
 }
-printf("\n");
 va_end(ap);
+printf("\n");
 }
