3741598cc3b1774697650bb25935c59e1e5c23e0
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-03-19 20:23:14 +0000

    :monocle_face: Possible solution

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,18 +15,13 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-		{
 			printf("[0] (nil)\n");
-			i++;
-			h = h->next;
-		}
 		else
-		{
 			printf("[%d] %s\n", h->len, h->str);
-			i++;
-			h = h->next;
-		}
-}
+
+		i++;
+		h = h->next;
+	}
 
 	return (i);
 }
