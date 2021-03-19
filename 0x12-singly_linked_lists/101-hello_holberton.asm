abd8eb315624588cd196c632af1a660e1358bfce
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-03-19 17:38:54 +0000

    :seedling: add task

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,18 @@
+SECTION .data
+
+	fmt db "%s", 10, 0
+	msg db "Hello, Holberton"
+
+SECTION .text
+	extern printf
+	global main
+
+main:
+	mov  esi, msg
+	mov  edi, fmt
+	mov  eax, 0
+	call printf
+
+	mov ebx, 0
+	mov eax, 1
+	int 0x80
