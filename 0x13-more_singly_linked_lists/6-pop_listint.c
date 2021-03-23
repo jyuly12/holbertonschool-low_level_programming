0b9cc0b8ecd96bd450a5ee7ea21de4da7d347161
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-03-23 18:44:36 +0000

    :seedling: add task 6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the head node of a listint_t linked list.
+ * @head: listint_t linked list.
+ *
+ * Return: head nodes data.
+ */
+int pop_listint(listint_t **head)
+{
+	int i = 0;
+
+	i = (*head)->n;
+	*head = (*head)->next;
+	free(head);
+
+	return (i);
+}
