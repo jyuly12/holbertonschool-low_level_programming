9a92277afb00abb4f5b8526c47f957c4998a7618
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-03-23 16:22:45 +0000

    :seedling: add task 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+B#include "lists.h"
+
+/**
+ * listint_len - count number of elements in a linked listint_t list.
+ * @h: pointer to linked list.
+ *
+ * Return: size of listint_t.
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	int i = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
