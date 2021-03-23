bf5392ffc6eccf2ab92f876f7bf3484cf1d9e1c9
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-03-23 17:23:31 +0000

    :seedling: add task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a list and sets head to NULL
+ * @head: list of type listint_t
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *list;
+
+	while (*head != NULL)
+	{
+		list = (*head)->next;
+		free(*head);
+		*head = list;
+	}
+}
