19f209839c3222ab9fca33d0fc4821d59d571cf7
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-03-30 03:52:31 +0000

    :seedling: add task 3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * main - copies the content of a file to another file.
+ * @ac: argument.
+ * @av: array.
+ *
+ * Return: Always 0.
+ */
+
+int main(int ac, char **av)
+{
+	return (0);
+}
