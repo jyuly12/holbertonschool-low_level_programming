db0d2973c03be8c5a7416d154e382dd73a50838e
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-03-30 00:37:39 +0000

    :seedling: add task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * create_file - creates a file.
+ * @filename: file to create.
+ * @text_content: content to file.
+ *
+ * Return: 1 on success, -1 on failure.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, fn, i = 0;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+		if (fd == -1)
+			return (-1);
+	if (text_content)
+	{
+		while (text_content[i])
+			i++;
+		fn = write(fd, text_content, i);
+	}
+	if (fn == -1)
+		return (-1);
+	close(fd);
+	return (1);
+}
