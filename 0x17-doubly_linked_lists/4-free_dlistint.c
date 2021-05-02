4194a0decf9ee6d727fbd38ded05d411cb6fb2cc
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-05-02 04:01:12 +0000

    [:seedling:] task 4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees a dlistint_t list.
+ * @head: dlistint_t structure.
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	if (head != NULL)
+	{
+		free_dlistint(head->next);
+		free(head);
+	}
+}
