c1be9124d0d5ad2f55dc12f3816de6fdbf5ee73e
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-05-28 16:35:10 -0500

    [ADD] task 0 - 2 and readme

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+* hash_table_create - Creates a empty hash table
+* @size: size of the array
+*
+* Return: Pointer to the newly created hash table, otherwise NULL.
+*/
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+    hash_table_t *hash_table = malloc(sizeof(hash_table_t));
+    unsigned long int i = 0;
+
+    if (!hash_table)
+        return (NULL);
+
+    hash_table->size = size;
+    hash_table->array = malloc(sizeof(hash_node_t *)* size);
+
+    if (!hash_table->array)
+    {
+        free(hash_table);
+        return (NULL);
+    }
+
+    while (i < size)
+    {
+        hash_table->array[i] = NULL;
+        i++;
+    }
+    return (hash_table);
+}
