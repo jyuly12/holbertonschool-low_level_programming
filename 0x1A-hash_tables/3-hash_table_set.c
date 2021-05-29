baae7a57475add33d8ebf829f77688e5b29459f1
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-05-28 20:59:07 -0500

    [FIX] Betty

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -31,7 +31,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
         }
         tmp_node = tmp_node->next;
     }
-    
+
     new_node = malloc(sizeof(hash_node_t));
     if (new_node == NULL)
     {
